40cf318317c3d108ae3501ac1994f5de9066b057
d-correalr <d.correalr@uniandes.edu.co>
2025-05-24 22:52:35 -0500

    retoques finales api

--- api_peliculas.py
+++ api_peliculas.py
@@ -3,18 +3,25 @@ from flask_restx import Api, Resource, reqparse
 from model_backend import predict_genre  # asegúrate que este nombre coincida
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='API Predicción de Género de Películas')
 
-ns = api.namespace('predict', description='Predicción')
+api = Api(
+    app,
+    version='1.0',
+    title='Predicción Género de una Película',
+    description='API que predice el género de una película con base en algunas características',
+    mask=None
+)
 
-parser = reqparse.RequestParser()
-parser.add_argument('year', type=float, required=True)
-parser.add_argument('rating', type=float, required=True)
-parser.add_argument('title', type=str, required=True)
-parser.add_argument('plot', type=str, required=True)
+ns = api.namespace('predict', description='Predicción de género de películas')
+
+parser = reqparse.RequestParser(trim=True)
+parser.add_argument('year', type=float, required=True, help='Año de la película (1984 a 2015)', location='args')
+parser.add_argument('rating', type=float, required=True, help='rating de la película (1.2 a 9.3)', location='args')
+parser.add_argument('title', type=str, required=True, help='Título de la película', location='args')
+parser.add_argument('plot', type=str, required=True, help='Trama de la película', location='args')
 
 @ns.route('/')
-class GenreAPI(Resource):
+class GenreApi(Resource):
     @api.expect(parser)
     def get(self):
         args = parser.parse_args()
@@ -22,10 +29,14 @@ class GenreAPI(Resource):
             result = predict_genre(**args)
             return {
                 "input": args,
-                "prediction": result
+                "prediction": {
+                    "genre": result["predicted_genre"],
+                    "top_5_genres": result["top_5_genres"]
+                }
             }, 200
-        except Exception as e:
-            return {"message": str(e)}, 400
+        except ValueError as e:
+            return {'message': str(e)}, 400
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+

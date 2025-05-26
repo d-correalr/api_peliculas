01648d1f78731df075211b71e15d23a03bdd8e49
d-correalr <d.correalr@uniandes.edu.co>
2025-05-25 21:05:19 -0500

    prueba API2

---
+++ api_peliculas_copia.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_restx import Api, Resource, reqparse
+from model_backend import predict_genre 
+
+app = Flask(__name__)
+
+api = Api(
+    app,
+    version='1.0',
+    title='Predicción Género de una Película',
+    description='API que predice el género de una película con base en algunas características',
+    mask=None
+)
+
+ns = api.namespace('predict', description='Predicción de género de películas')
+
+parser = reqparse.RequestParser(trim=True)
+parser.add_argument('year', type=float, required=True, help='Año de la película (1984 a 2015)', location='args')
+parser.add_argument('rating', type=float, required=True, help='rating de la película (1.2 a 9.3)', location='args')
+parser.add_argument('title', type=str, required=True, help='Título de la película', location='args')
+parser.add_argument('plot', type=str, required=True, help='Trama de la película', location='args')
+
+@ns.route('/')
+class GenreApi(Resource):
+    @api.expect(parser)
+    def get(self):
+        args = parser.parse_args()
+        try:
+            result = predict_genre(**args)
+            return {
+                "input": args,
+                "prediction": {
+                    "genre": result["predicted_genre"],
+                    "top_5_genres": result["top_5_genres"]
+                }
+            }, 200
+        except ValueError as e:
+            return {'message': str(e)}, 400
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
---
+++ model_backend _2.py
@@ -0,0 +1,41 @@
+import pickle
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+# Cargar el modelo ensamblado
+with open("modelo_ensamble_final.pkl", "rb") as f:
+    modelo = pickle.load(f)
+
+clf_sbert = modelo["clf_sbert"]
+clf_tfidf = modelo["clf_tfidf"]
+vectorizer = modelo["vectorizer"]
+mlb = modelo["mlb"]
+mejor_peso = modelo["mejor_peso"]
+cols = modelo["cols"]
+
+embedder = SentenceTransformer("all-MiniLM-L6-v2")
+
+def predict_genre(year, rating, title, plot):
+    texto = plot.lower()
+
+    # Vectorizaciones
+    emb = embedder.encode([texto])
+    tfidf_vec = vectorizer.transform([texto])
+
+    # Predicciones
+    probs_sbert = clf_sbert.predict_proba(emb)
+    probs_tfidf = clf_tfidf.predict_proba(tfidf_vec)
+    probs_final = mejor_peso * probs_sbert + (1 - mejor_peso) * probs_tfidf
+    probs_final = probs_final.flatten()
+
+    # Top 5 géneros
+    top_idxs = np.argsort(probs_final)[::-1][:5]
+    top_genres = [
+        {"genre": cols[i].replace("p_", ""), "probability": float(probs_final[i])}
+        for i in top_idxs
+    ]
+
+    return {
+        "predicted_genre": top_genres[0]["genre"],
+        "top_5_genres": top_genres
+    }
